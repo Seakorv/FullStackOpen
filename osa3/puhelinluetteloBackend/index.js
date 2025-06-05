0dbefe67201e1d0ad5cfd9fb3cd641b135fed7d7
Seakorv <arttudun@gmail.com>
2025-06-05 19:43:11 +0300

    3.14 (ja 3.15) tehtynä

--- osa3/puhelinluetteloBackend/index.js
+++ osa3/puhelinluetteloBackend/index.js
@@ -1,43 +1,50 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
 const app = express()
+
+let persons = []
 
 morgan.token('postContent', (req) => {
   return JSON.stringify(req.body)
 })
 
-app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postContent'))
 app.use(express.static('dist'))
+app.use(express.json())
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-]
-
-const generateId = () => {
+/*const generateId = () => {
   const min = 5;
   const max = 6942069420;
   return Math.floor(Math.random() * (max - min) + min);
-}
+}*/
+
+app.get('/', (request, response) => {
+  response.send('<h1>Puhelinluettelo</h1>')
+})
+
+app.get('/info', (request, response) => {
+  const date = new Date();
+  response.send(
+    `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+    `)
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  Person.findById(request.params.id).then((person) => {
+    response.json(person)
+  })
+})
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
@@ -58,54 +65,31 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  const person = {
-    id: generateId().toString(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
+  })
 
-  persons = persons.concat(person)
-
-  //console.log(person)
-  response.json(person)
-})
-
-app.get('/', (request, response) => {
-  response.send('<h1>Puhelinluettelo</h1>')
-})
-
-app.get('/info', (request, response) => {
-  const date = new Date();
-  response.send(
-    `
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${date}</p>
-    `)
-})
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
---
+++ osa3/puhelinluetteloBackend/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length <3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+const url = `mongodb+srv://arttudun:${password}@cluster0.y0i0vwh.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: personName,
+  number: personNumber,
+})
+
+if (personName != null && personNumber != null) {
+  person.save().then(result => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`)
+    mongoose.connection.close()
+    })
+} else {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
